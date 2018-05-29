b2e79efcb8bfb0db7095db7d397615480b79e3de
Kareem Belgharbi <kareembelgharbi@gmail.com>
2018-05-29 18:13:11 +0200

    Chat

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 require('dotenv').load();
 
+// Express routing
 app.get('/', function(req, res) {
 	res.send('Hello World!');
 });
 
-app.listen(3000, function() {
-	console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function() {
+	console.log('App.js is running on port 8080.');
+});
+
+// socket.io
+http.listen(process.env.PORT, function(){
+	console.log(process.env.PORT);
+});
+
+io.on('connection', function(socket){
+	socket.on('message', function(msg){
+			console.log(msg.name + ': ' + msg.message);
+			io.emit('message', msg);
+	});
 });
