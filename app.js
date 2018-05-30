fe7a2787888de075d564ac2d4c55c3f5d96a2e63
Kareem Belgharbi <kareembelgharbi@gmail.com>
2018-05-30 15:26:32 +0200

    Saves last 10 chat messages

--- app.js
+++ app.js
@@ -6,23 +6,45 @@ const io = require('socket.io')(http);
 
 require('dotenv').load();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mysql = require("mysql");
+
 // Express routing
 app.get('/', function(req, res) {
 	res.send('Hello World!');
 });
 
+app.get('/chat', function(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.send(lastMessages);
+});
+
 app.listen(process.env.PORT, function() {
-	console.log('App.js is running on port 8080.');
+	console.log(`App.js is listening on port ${process.env.PORT}.`);
 });
 
 // socket.io
-http.listen(process.env.PORT, function(){
-	console.log(process.env.PORT);
+http.listen(process.env.SOCKET_IO_PORT, function() {
+	console.log(`Socket.io is listening on port ${process.env.SOCKET_IO_PORT}.`);
 });
 
-io.on('connection', function(socket){
-	socket.on('message', function(msg){
-			console.log(msg.name + ': ' + msg.message);
+io.on('connection', function(socket) {
+	socket.on('message', function(msg) {
+			insertMessage(msg);
 			io.emit('message', msg);
 	});
 });
+
+var lastMessages = [];
+function insertMessage(msg) {
+	console.log(msg.name + ': ' + msg.message);
+
+	lastMessages.push(msg);
+	if (lastMessages.length > 10) lastMessages.shift();
+}
