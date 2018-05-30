92c15f46b16b6976a5341caec270d91efb5ab4d4
Kareem Belgharbi <kareembelgharbi@gmail.com>
2018-05-30 16:48:57 +0200

    Chat final

--- app.js
+++ app.js
@@ -17,16 +17,20 @@ app.get('/', function(req, res) {
 	res.send('Hello World!');
 });
 
+app.listen(process.env.PORT, function() {
+	console.log(`App.js is listening on port ${process.env.PORT}.`);
+});
+
+// Sends the last 10 chat messages.
 app.get('/chat', function(req, res) {
 	res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.send(lastMessages);
-});
-
-app.listen(process.env.PORT, function() {
-	console.log(`App.js is listening on port ${process.env.PORT}.`);
+	
+	queryDB('SELECT * FROM chat_messages LIMIT 10', [], (result) => {
+		res.send(result);
+	});
 });
 
 // socket.io
@@ -36,21 +40,12 @@ http.listen(process.env.SOCKET_IO_PORT, function() {
 
 io.on('connection', function(socket) {
 	socket.on('message', function(msg) {
-			insertMessage(msg);
-			io.emit('message', msg);
+			var values = [[msg.name, msg.profileImage, msg.message]];
+			queryDB('INSERT INTO chat_messages (name, image, content) VALUES ?', [values], (result) => {
+				io.emit('message', msg);
+			});
 	});
 });
-
-var lastMessages = [];
-function insertMessage(msg) {
-	console.log(msg.name + ': ' + msg.message);
-
-	var values = [[msg.name, msg.profileImage, msg.message]];
-	queryDB('INSERT INTO chat_messages (name, image, content) VALUES ?', [values], (result) => {
-		console.log(result);
-	});
-}
-
 
 function queryDB(sql, args, callback) {
 	var config = {
